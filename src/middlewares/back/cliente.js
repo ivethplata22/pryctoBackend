1473a032b7caff69f64d76ca1a0cf79641b026be
IvethPlata <karen.platahdz22@outlook.es>
2024-10-07 20:37:25 -0600

    Validar que venga la informacion necesaria en los endpoints

--- src/middlewares/back/cliente.js
+++ src/middlewares/back/cliente.js
@@ -6,7 +6,13 @@ const { validateFields } = require('../validateFields');
 // Validaciones
 const MiddleCliente = {
     actualizarCliente: [
-        
+        check('id_cliente', 'El id del cliente es obligatorio').notEmpty(),
+        check('nombre', 'El nombre del cliente es obligatorio').notEmpty(),
+        check('email', 'El correo electronico es obligatorio').notEmpty(),
+        check('telefono', 'El numero de telefono es obligatorio').notEmpty(),
+        check('direccion', 'La direccion es obligatoria').notEmpty(),
+        check('ingresomensual', 'El ingreso mensual es obligatorio').notEmpty(),
+        validateFields
     ],
 };
 
--- src/middlewares/back/credito.js
+++ src/middlewares/back/credito.js
@@ -10,20 +10,26 @@ const MiddleCredito = {
         check('email', 'El correo electronico es obligatorio').notEmpty(),
         check('telefono', 'El numero de telefono es obligatorio').notEmpty(),
         check('direccion', 'La direccion es obligatoria').notEmpty(),
-        check('ingresoM', 'El ingreso mensual es obligatorio').notEmpty(),
+        check('ingresomensual', 'El ingreso mensual es obligatorio').notEmpty(),
         check('id_sucursal', 'El id de la sucursal es obligatorio').notEmpty(),
         check('monto', 'El monto solicitado es obligatorio').notEmpty(),
         check('plazo', 'Plazo a meses en que se pagara').notEmpty(),
         validateFields
     ],
     solicitud: [
-        
+        check('uuidcliente', 'El identificador de cliente es obligatorio').notEmpty(),
+        check('id_sucursal', 'El id de la sucursal es obligatorio').notEmpty(),
+        check('monto', 'El monto solicitado es obligatorio').notEmpty(),
+        check('plazo', 'Plazo a meses en que se pagara').notEmpty(),
+        validateFields
     ],
     solicitudesCredito: [
-
+        check('solicitudes', 'El arreglo de solicitudes es obligatorio').notEmpty(),
+        validateFields
     ],
     obtenerSolicitudes: [
-        
+        check('id_cliente', 'El id del cliente es obligatorio').notEmpty(),
+        validateFields
     ]
 };
 
